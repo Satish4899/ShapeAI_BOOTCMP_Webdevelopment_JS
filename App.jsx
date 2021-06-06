8b8aea692f9083f94418c6dc6f7039331924121b
Satish4899 <85427816+Satish4899@users.noreply.github.com>
2021-06-06 14:14:55 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import about from "./about";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+      <Note />
+
+      <about />
+    </div>
+  );
+}
+
+export default App;
---
+++ about.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about">
+      <h2>About Bootcamp</h2>
+      <p>
+        It was fun learning with him. I hope there will be many such Bootcamps
+        in future.
+      </p>
+    </div>
+  );
+}
+
+export default About;
